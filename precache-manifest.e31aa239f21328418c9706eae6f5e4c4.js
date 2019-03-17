80c8abb515e9c443ce3f4d582c5a6529ad4edc2c
Jayesh Khullar <jayeshkhullar@gmail.com>
2019-03-17 14:07:05 -0400

    Updates

---
+++ precache-manifest.e31aa239f21328418c9706eae6f5e4c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11ec10a5d0e2c70425e500399b47dd97",
+    "url": "/jayesh/static/media/uoft.11ec10a5.svg"
+  },
+  {
+    "revision": "3aecac2e68ca8623dddb",
+    "url": "/jayesh/static/js/runtime~main.03e738a9.js"
+  },
+  {
+    "revision": "56faedb15581cf7adf69",
+    "url": "/jayesh/static/js/main.f1de52d1.chunk.js"
+  },
+  {
+    "revision": "94b63b9fb6534dc51f18",
+    "url": "/jayesh/static/js/2.0b099c1a.chunk.js"
+  },
+  {
+    "revision": "56faedb15581cf7adf69",
+    "url": "/jayesh/static/css/main.87818cbf.chunk.css"
+  },
+  {
+    "revision": "7e7fa858a2d7b49850709314e59ffd40",
+    "url": "/jayesh/index.html"
+  }
+];
