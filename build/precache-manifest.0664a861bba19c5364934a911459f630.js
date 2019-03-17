107b750885f371277ca3c80d1f50160217ff9507
Jayesh Khullar <jayeshkhullar@gmail.com>
2019-03-17 13:35:58 -0400

    add build

---
+++ build/precache-manifest.0664a861bba19c5364934a911459f630.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11ec10a5d0e2c70425e500399b47dd97",
+    "url": "/static/media/uoft.11ec10a5.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c6b76eca8df1fa4e249b",
+    "url": "/static/js/main.ecd7fe0e.chunk.js"
+  },
+  {
+    "revision": "94b63b9fb6534dc51f18",
+    "url": "/static/js/2.0b099c1a.chunk.js"
+  },
+  {
+    "revision": "c6b76eca8df1fa4e249b",
+    "url": "/static/css/main.87818cbf.chunk.css"
+  },
+  {
+    "revision": "14ca37bbb09883eb8fb678c2910d90de",
+    "url": "/index.html"
+  }
+];
