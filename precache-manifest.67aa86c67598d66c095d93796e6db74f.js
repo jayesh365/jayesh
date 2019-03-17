b5ce747bc9aaeab5fb798a3931928abb3ab7a612
Jayesh Khullar <jayeshkhullar@gmail.com>
2019-03-17 14:03:28 -0400

    Updates

---
+++ precache-manifest.67aa86c67598d66c095d93796e6db74f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11ec10a5d0e2c70425e500399b47dd97",
+    "url": "/jayesh365.github.io/static/media/uoft.11ec10a5.svg"
+  },
+  {
+    "revision": "5c41e1462b7825150803",
+    "url": "/jayesh365.github.io/static/js/runtime~main.c7c805ef.js"
+  },
+  {
+    "revision": "f685401ac2fd6ec32511",
+    "url": "/jayesh365.github.io/static/js/main.bf752a5e.chunk.js"
+  },
+  {
+    "revision": "94b63b9fb6534dc51f18",
+    "url": "/jayesh365.github.io/static/js/2.0b099c1a.chunk.js"
+  },
+  {
+    "revision": "f685401ac2fd6ec32511",
+    "url": "/jayesh365.github.io/static/css/main.87818cbf.chunk.css"
+  },
+  {
+    "revision": "e9b3838d507d8f8776c3f63ddad340d2",
+    "url": "/jayesh365.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0664a861bba19c5364934a911459f630.js"
+  "/jayesh365.github.io/precache-manifest.67aa86c67598d66c095d93796e6db74f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/jayesh365.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
