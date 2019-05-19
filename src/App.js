acd4d331edc65d28c75153e170983b0f4eda8244
Jayesh Khullar <jayeshkhullar@gmail.com>
2019-05-19 14:45:20 -0400

    added base template

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'; 
 import './App.css';
 import Square from './components/base.js';
+import './logo.svg'
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <Square content="Hello Test!"/>
+        <Square 
+        src='takephoto.jpg'
+        title="Jayesh Khullar" 
+        blurb="Hey!
+        I am a Machine Learning and Data Mining student at UoftT Scarborough. 
+        I am currently working on projects using Python, Java, Go, C++."
+        />
       </div>
     );
   }
--- src/components/base.js
+++ src/components/base.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 function Square(props){
         return (
-            <div id="main">
-                <div className="square">
-                <p>{props.content}</p>
+            <div className="base">
+                <div className="flex-square">
+                    <div className="left-column">
+                        <img className="c-image" src={props.src} alt="pic"></img>
+                    </div>
+                    <div className="content">
+                        <h1 className="title">{props.title}</h1>
+                        <p className="blurb">{props.blurb}</p>
+                    </div>
                 </div>
             </div>
         );
