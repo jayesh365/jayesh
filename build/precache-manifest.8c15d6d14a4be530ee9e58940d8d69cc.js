78ee67516cf91710b5c91a7e51496c6752b8f636
Jayesh Khullar <jayeshkhullar@gmail.com>
2019-05-19 14:46:42 -0400

    new build

---
+++ build/precache-manifest.8c15d6d14a4be530ee9e58940d8d69cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3aecac2e68ca8623dddb",
+    "url": "/jayesh/static/js/runtime~main.03e738a9.js"
+  },
+  {
+    "revision": "a0126c212ca78cc3338c",
+    "url": "/jayesh/static/js/main.66d520af.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/jayesh/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "a0126c212ca78cc3338c",
+    "url": "/jayesh/static/css/main.0f29e5a5.chunk.css"
+  },
+  {
+    "revision": "9b9fd72a96fdbb864c7b9ba8cec007b6",
+    "url": "/jayesh/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/jayesh/precache-manifest.b528ddd0e301852869cb5e159fed6fbe.js"
+  "/jayesh/precache-manifest.8c15d6d14a4be530ee9e58940d8d69cc.js"
 );
 
 workbox.clientsClaim();
